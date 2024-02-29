d8a0f931847da50ad2359b6d0b4709294924268e
iiRyan <92063117+iiRyan@users.noreply.github.com>
2024-02-29 19:23:33 +0300

    Made improvement to BudgetSummaryDao and Budget.html.

--- salary-tracker/src/main/java/com/rayan/salarytracker/dao/BudgetSummaryDao.java
+++ salary-tracker/src/main/java/com/rayan/salarytracker/dao/BudgetSummaryDao.java
@@ -8,7 +8,7 @@ import org.springframework.data.repository.query.Param;
 
 public interface BudgetSummaryDao extends JpaRepository<BudgetSummary, Long> {
 
-    @Query(value = "SELECT s.salary_id AS salary_id, s.salary AS total_salary, COALESCE(SUM(b.budget_amount), 0) AS total_expenses, (s.salary - COALESCE(SUM(b.budget_amount), 0)) AS remaining_salary FROM  salary s LEFT JOIN  budget b ON s.salary_id = b.salary_id WHERE  s.salary_id = :salaryId GROUP BY  s.salary_id, s.salary", nativeQuery = true)
+    @Query(value = "SELECT s.salary_id AS salary_id, COALESCE(SUM(b.budget_amount), 0) AS total_expenses, (s.salary - COALESCE(SUM(b.budget_amount), 0)) AS remaining_salary FROM salary s LEFT JOIN budget b ON s.salary_id = b.salary_id WHERE s.salary_id =:salaryId GROUP BY s.salary_id,s.salary", nativeQuery = true)
     BudgetSummary findBySalaryId(@Param("salaryId") Long salaryId);
 
 }
